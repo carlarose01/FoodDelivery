127431e31181c8890b41c81f8f04c8f4fe2e4efc
Carla Rose <57645881+carlarose01@users.noreply.github.com>
2020-11-23 12:16:52 -0500

    Initialized Parse database

---
+++ app/src/main/java/com/example/safecrowd/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.safecrowd;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("jpyd5CaQ2R7kePIDUMzuugJ2TrU49gPJCqsriZ3p")
+                .clientKey("X5hvnu4E1fLGrdUP4TQfwWNCaKge1LAoSWbw1IJI")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
